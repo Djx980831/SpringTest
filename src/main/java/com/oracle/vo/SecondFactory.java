c110616d92a13a456b7a8076f187497843d837f8
Djx980831 <1530916212@qq.com>
2019-01-07 20:41:08 +0800

    利用工厂创建产品的两种方法 1.简单工厂创建对象（单例也是可用这种方法） 2.创建出工厂，工厂方法创建出来的对象

---
+++ src/main/java/com/oracle/vo/SecondFactory.java
@@ -0,0 +1,7 @@
+package com.oracle.vo;
+
+public class SecondFactory {
+    public Weapon getWeapon(){
+        return new Weapon("长剑",13);
+    }
+}
--- src/main/java/com/oracle/vo/SimpleFactory.java
+++ src/main/java/com/oracle/vo/SimpleFactory.java
@@ -1,8 +1,13 @@
 package com.oracle.vo;
 
 public class SimpleFactory {
-    public static Weapon getWeapon(){
-        return new Weapon("诸葛连弩",44);
+    public static Weapon getWeapon(int a){
+        if(a == 0){
+            return new Weapon("诸葛连弩",44);
+        }else {
+            return new Weapon("穿云箭",55);
+        }
+
     }
 
 }
